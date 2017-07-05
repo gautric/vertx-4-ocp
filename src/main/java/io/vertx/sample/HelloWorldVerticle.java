5bd64116d03971dc9a29a0b7c06d5d304a8b0091
gautric <gautric@redhat.com>
2017-07-05 11:32:41 +0200

    add VERTX_4_OCP_MESSAGE

--- src/main/java/io/vertx/sample/HelloWorldVerticle.java
+++ src/main/java/io/vertx/sample/HelloWorldVerticle.java
@@ -6,8 +6,16 @@ public class HelloWorldVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
+    String message = System.getenv("VERTX_4_OCP_MESSAGE");
+
+    if(message==null || message.length()==0){
+      message = "Hello world !!";
+    }
+
+    System.out.println("Message " + message);
+
     vertx.createHttpServer()
-      .requestHandler(req -> req.response().end("Hello World ! おはようございます"))
+      .requestHandler(req -> req.response().end(message))
       .listen(8080);
   }
 }
